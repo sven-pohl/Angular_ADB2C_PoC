2c4e0344f3d98aabc93ec2b86af53e885bea63bd
Sven Pohl <sven.pohl@microsoft.com>
2019-11-28 16:07:03 +0100

    Add webapi

---
+++ demo.api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace demo.api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetAsync() => Ok("Hello, World!");
+    }
+}
