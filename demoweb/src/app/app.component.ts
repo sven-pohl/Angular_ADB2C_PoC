cb3f812bfc4e7de8144b8b48cb98bd97d38fd5af
Sven Pohl <sven.pohl@microsoft.com>
2020-07-08 09:42:26 +0200

    update demoweb to angular 10

--- demoweb/src/app/app.component.ts
+++ demoweb/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { OAuthService, NullValidationHandler, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+import { OAuthService, NullValidationHandler } from 'angular-oauth2-oidc';
 import { authConfig, DiscoveryDocumentConfig, authConfigPkce } from './auth.config';
 
 @Component({
